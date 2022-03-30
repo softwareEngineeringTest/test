8ce45ecf1ed5fc2b12ce934703a5992732b32fc2
sohyun-99 <sophia4130@naver.com>
2022-03-30 16:29:33 +0900

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
 using namespace std;
 int main() {
+   int count;
+   cin >> count;
+   for (int i = 0; i < count; i++) {
+      for (int j = 1; j <= i; j++)
+      {
+         cout << " ";
+      }
+      for (int j = 1; j <= count - i; j++)
+      {
+         cout << "*";
+      } cout << "아아아아\n";
+        cout << "\n";
+   }
    
+}
+
+void test(){
+    for (int j = 1; j <= 10; j++)
+      {
+         cout << " ";
+      }
 }
